5616d4039c0c245db3b1bf4ba7c3dbb40a218f88
tgalex75 <81436086+tgalex75@users.noreply.github.com>
2024-12-16 22:44:49 +0100

    Update FetchImprevisto.js

--- src/Funzioni/FetchImprevisto.js
+++ src/Funzioni/FetchImprevisto.js
@@ -1,28 +1,20 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import useFetchData from "../Hooks/useFetchData";
 import { supabase } from "../supabaseClient";
 import capitalize from "lodash.capitalize";
+import { MdOutlineSnooze } from "react-icons/md";
+import random from "random";
 
 export default function FetchImprevisto() {
-  const [imprevisto, setImprevisto] = useState([]);
+  const { data } = useFetchData("imprevisti");
 
-  useEffect(() => {
-    fetchLista();
-  }, []);
+  const [isSaved, setIsSaved] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
       delElemento();
     }, 3000);
   });
-
-  const fetchLista = async () => {
-    const { data } = await supabase
-      .from("random_sort")
-      .select("*")
-      .limit(1)
-      .single();
-    setImprevisto(data ? data : { id: 0, descrizione: "LISTA VUOTA!!!" });
-  };
 
   const delElemento = async () => {
     const { error } = await supabase
@@ -32,14 +24,20 @@ export default function FetchImprevisto() {
     error && console.log(error);
   };
 
-  const { id, titolo, descrizione } = imprevisto;
+  const casuale = random.choice(data) || {
+    id: 0,
+    descrizione: "LISTA VUOTA!!!",
+  };
+
+  const { id, titolo, descrizione } = casuale;
 
   const rimandaImprevisto = async () => {
     const { error } = await supabase
       .from("salvaxdopo")
-      .insert([{id: id, titolo: titolo, descrizione: descrizione }])
+      .insert([{ id: id, titolo: titolo, descrizione: descrizione }])
       .select();
     error && console.log(error);
+    setIsSaved(true);
   };
 
   return (
@@ -62,10 +60,23 @@ export default function FetchImprevisto() {
       </p>
       <button
         onClick={rimandaImprevisto}
-        className="mt-8 h-10 w-1/5 self-end rounded-lg px-2 py-2 text-center text-sm font-bold text-gray-300 shadow-md transition duration-200 ease-in hover:bg-purple-700"
+        className="peer mt-16 rounded-full p-2 text-center text-sm font-bold shadow-md transition duration-200 ease-in hover:scale-125 hover:bg-purple-700 hover:text-gray-300"
       >
-        Posticipa imprevisto?
+        <MdOutlineSnooze size={36} />
       </button>
+      {!isSaved ? (
+        <span
+          className="peer-hover:visible invisible text-xs transition-all duration-150 ease-in-out"
+        >
+          Posticipa e salva imprevisto?
+        </span>
+      ) : (
+        <span
+          className="text-xs transition-all duration-150 ease-in-out"
+        >
+          Imprevisto posticipato e salvato!
+        </span>
+      )}
     </section>
   );
 }
