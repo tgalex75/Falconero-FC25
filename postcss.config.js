a7c7cfc4dc5dfc2901ee99aa97e59ae814e072e1
tgalex75 <81436086+tgalex75@users.noreply.github.com>
2025-04-14 20:20:15 +0200

    Fixed Tailwindcss
    Downgrade package

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    plugins: [
-      require('@tailwindcss/postcss')(),
-      require('autoprefixer'),
-    ],
-  };
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  }
   
